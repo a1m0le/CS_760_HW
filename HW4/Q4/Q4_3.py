8a30bc1aa7f0d846074e4c744a9266933bc1298a
Matt Xu <xzy1998@cs.washington.edu>
2023-10-28 10:21:12 -0500

    [M] Time to change my submission PDF

--- HW4/Q4/Q4_3.py
+++ HW4/Q4/Q4_3.py
@@ -33,39 +33,7 @@ def load_testing_data():
 
 
 
-
-def torchyTRAIN(training_data, batchlimit=None):
-    # train using the power of pytorch
-    torchNN = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(784,300,bias=False),
-            nn.Sigmoid(),
-            nn.Linear(300,10,bias=False),
-    )
-    pytorchSGD = torch.optim.SGD(torchNN.parameters(), lr=hp.LEARNING_RATE)
-    CELoss = nn.CrossEntropyLoss() 
-    torchNN.train()
-    if batchlimit is not None and batchlimit == 0:
-        return torchNN
-    for epoch in range(1, hp.EPOCH+1):
-        total_loss = 0
-        batchcount = 0
-        for batchdata, labels in training_data:
-            pytorchSGD.zero_grad()
-            forward_out = torchNN(batchdata)
-            loss= CELoss(forward_out, labels)
-            loss.backward()
-            pytorchSGD.step()
-            batchcount += 1
-            if batchlimit is not None and batchlimit == batchcount:
-                break
-    return torchNN       
-
-
-def evaluate_torchy(testing_data, torchNN):
-    correct_ones = 0
-    total_loss = 0
-    avg_factor = 0
+def torchyEVAL(testing_data, torchNN):
     CELoss = nn.CrossEntropyLoss()
     torchNN.eval()
     with torch.no_grad():
@@ -87,39 +55,67 @@ def evaluate_torchy(testing_data, torchNN):
                         uniform = False
                 if maxprob == truthprob and not uniform:
                     correct_ones += 1
-    return correct_ones/avg_factor, total_loss/avg_factor
+    accu = correct_ones / avg_factor
+    loss = total_loss / avg_factor
+    accu_text = "{:.2f}%".format(accu*100)
+    loss_text = "{:.4f}".format(loss)
+    print("Accuracy= "+accu_text+" Loss = "+loss_text,end="")
+    return accu
 
+
+def torchyTRAIN(training_data, testing_data):
+    # train using the power of pytorch
+    torchNN = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(784,300,bias=False),
+            nn.Sigmoid(),
+            nn.Linear(300,10,bias=False),
+    )
+    pytorchSGD = torch.optim.SGD(torchNN.parameters(), lr=hp.LEARNING_RATE)
+    CELoss = nn.CrossEntropyLoss() 
+    x_axis = []
+    test_accu_axis = []
+    print("Before training: Test ",end="")
+    first_test_accu = torchyEVAL(testing_data, torchNN)
+    x_axis.append(0)
+    test_accu_axis.append(first_test_accu)
+    print()
+    # Now we train
+    for epoch in range(1, hp.EPOCH+1):
+        torchNN.train()
+        for batchdata, labels in training_data:
+            pytorchSGD.zero_grad()
+            forward_out = torchNN(batchdata)
+            loss= CELoss(forward_out, labels)
+            loss.backward()
+            pytorchSGD.step()
+        # complete one epoch. evaluate
+        print("Epoch "+str(epoch)+": Test ",end="")
+        test_accu = torchyEVAL(testing_data, torchNN)
+        x_axis.append(epoch)
+        test_accu_axis.append(test_accu)
+        print()
+    return x_axis, test_accu_axis
 
 
 
 if __name__=="__main__":
     train_dl = load_training_data()
     test_dl = load_testing_data()
-    # train with different number of batches
-    x_axis = []
-    accu_axis = []
-    for multiplier in range(0, hp.AXIS_COUNT+1):
-        torchNN = torchyTRAIN(train_dl, batchlimit=multiplier*hp.GAP)
-        accu, loss = evaluate_torchy(test_dl, torchNN)
-        x_axis.append(multiplier * hp.GAP * hp.BATCHSIZE)
-        accu_axis.append(accu)
-        precent_text = "{:.2f}%".format(multiplier*hp.GAP/len(train_dl)*100)
-        accu_text = "{:.2f}%".format(accu*100)
-        err_text = "{:.2f}%".format((1-accu)*100)
-        print("Train with "+precent_text+" of data:  Accuracy = "+accu_text+"    Error rate = "+err_text+"   Avg.loss = "+str(loss))
+    x_axis, test_accu_axis = torchyTRAIN(train_dl, test_dl)
     # draw the curve
     print("=========  Curve data  ===========")
     print(x_axis)
-    print(accu_axis)
+    print(test_accu_axis)
+    # Time to plot
     fig, axes = plt.subplots()
     fig.set_figheight(6)
     fig.set_figwidth(10)
-    axes.set_xlim(0,60000)
+    axes.set_xlim(0,36)
     axes.set_ylim(0,1)
     axes.grid()
-    axes.set_xlabel("Number of data points used in the training")
+    axes.set_xlabel("Epoch number")
     axes.set_ylabel("Accuracy(%)")
-    axes.set_title("Accuracy on the test set when changing the size of training set\nModel = torchNN with powers of PyTorch\n(learning rate = "+str(hp.LEARNING_RATE)+"; batch size = "+str(hp.BATCHSIZE)+"; trained for "+str(hp.EPOCH)+" epochs each time)")
-    axes.plot(x_axis, accu_axis, linestyle='-', marker='*')
+    axes.set_title("Accuracy on the test set as we train the model\nModel = torchNN with powers of PyTorch\n(learning rate = "+str(hp.LEARNING_RATE)+"; batch size = "+str(hp.BATCHSIZE)+")")
+    axes.plot(x_axis, test_accu_axis, linestyle='-', marker='*')
     plt.savefig("Q4_3_learning_cruve.png")
-
